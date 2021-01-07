21ac6a47b754810e69845721169a78a0c8b5bb0c
David Santacoloma <adsi1407@gmail.com>
2021-01-07 15:25:12 -0500

    Agregar repositorio para manejar roles

---
+++ Domain/Domain/Repository/RoleRepository.swift
@@ -0,0 +1,11 @@
+//
+//  RoleRepository.swift
+//  Domain
+//
+//  Created by Andrés David Santacoloma Isaza - Ceiba Software on 7/01/21.
+//
+
+public protocol RoleRepository {
+    
+    func getRoles() -> [Role]
+}
---
+++ Domain/DomainTests/Service/PersonServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  PersonServiceTests.swift
+//  DomainTests
+//
+//  Created by Andrés David Santacoloma Isaza - Ceiba Software on 23/12/20.
+//
+
+import XCTest
+@testable import Domain
+
+class PersonServiceTests: XCTestCase {
+    
+    
+}
